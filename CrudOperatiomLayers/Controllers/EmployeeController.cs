0d1e426550a7671cdf1ddc2dde378d2465c6297c
Hasan73527 <79495585+Hasan73527@users.noreply.github.com>
2024-07-23 00:49:23 +0530

    EmployeeController.cs

--- CrudOperatiomLayers/Controllers/EmployeeController.cs
+++ CrudOperatiomLayers/Controllers/EmployeeController.cs
@@ -112,8 +112,6 @@ namespace CrudOperatiomLayers.Controllers
 		//	db.SaveChanges();
 		//}
 
-
-		//(This is the bad practise!) = > this should instead also call the BLL 
 		[Route("deleteEmployee")]
 		[HttpDelete]
 		public IActionResult deleteEmployee(int id)
@@ -133,108 +131,6 @@ namespace CrudOperatiomLayers.Controllers
 				return Ok("Employee details successfully deleted");
 			}
 		}
-		//public void deleteEmployee(int id)
-		//{
-		//	var db = new EmployeeDbContext();
-		//	Employee p = new Employee();
-		//	p = db.Employee.FirstOrDefault(x => x.Id == id);
-
-		//	if (p == null)
-		//		throw new Exception("Not found");
-
-		//	db.Employee.Remove(p);
-		//	db.SaveChanges();
-		//}
-
-
 	}
 }
-//{
-//	[ApiController]
-//	[Route("[controller]")]
-//	public class EmployeeController : ControllerBase
-//	{
 
-//		private Business_Logic_Layer.EmployeeBLL _BLL;
-//		public EmployeeController()
-//		{
-//			_BLL = new Business_Logic_Layer.EmployeeBLL();
-//		}
-
-
-//		[HttpGet]
-//		[Route("getEmployees")]
-
-
-//		public List<EmployeeModel> GetAllEmployees()
-//		{
-//			return _BLL.GetAllEmployees();
-//		}
-
-
-
-//		[HttpGet]
-//		[Route("getEmployee")]
-//		public ActionResult<EmployeeModel> GetEmployeeById(int id)
-//		{
-//			var Employee = _BLL.GetEmployeeById(id);
-
-//			if (Employee == null)
-//			{
-//				return NotFound("Invalid ID");
-//			}
-
-//			return Ok(Employee);
-//		}
-
-
-
-
-//		[Route("postEmployee")]
-//		[HttpPost]
-//		public void postEmployee([FromBody] EmployeeModel EmployeeModel)
-//		{
-//			_BLL.postEmployee(EmployeeModel);
-//		}
-
-//		[Route("updateEmployee")]
-//		[HttpPut]
-//		public void UpdateEmployee(int id, [FromBody] EmployeeModel updatedEmployee)
-//		{
-//			var db = new EmployeeDbContext();
-//			Employee existingEmployee = db.Employee.FirstOrDefault(x => x.Id == id);
-
-//			if (existingEmployee == null)
-//				throw new Exception("Not found");
-
-//			// Update the properties of the existing Employee with the values from the updatedEmployee object
-//			existingEmployee.FirstName = updatedEmployee.FirstName;
-//			existingEmployee.LastName = updatedEmployee.LastName;
-//			existingEmployee.Address = updatedEmployee.Address;
-//			existingEmployee.PhoneNumber = updatedEmployee.PhoneNumber;
-//			existingEmployee.DateOfBirth = updatedEmployee.DateOfBirth;
-//			// Add any other properties that need to be updated
-
-//			db.SaveChanges();
-//		}
-
-
-//		//(This is the bad practise!) = > this should instead also call the BLL 
-//		[Route("deleteEmployee")]
-//		[HttpDelete]
-//		public void deleteEmployee(int id)
-//		{
-//			var db = new EmployeeDbContext();
-//			Employee p = new Employee();
-//			p = db.Employee.FirstOrDefault(x => x.Id == id);
-
-//			if (p == null)
-//				throw new Exception("Not found");
-
-//			db.Employee.Remove(p);
-//			db.SaveChanges();
-//		}
-
-
-//	}
-//}
